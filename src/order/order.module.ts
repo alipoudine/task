8d5ee395190159317c1b28e94e6629918d20e02c
AliPoudine <ali.poudine99@gmail.com>
2023-04-17 16:59:33 +0330

    order model tests

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { VendingMachineModule } from 'src/vending-machine/vending-machine.module';
 import { OrderService } from './order.service';
 
 @Module({
-  providers: [OrderService]
+  providers: [OrderService],
+  imports: [VendingMachineModule],
 })
 export class OrderModule {}
